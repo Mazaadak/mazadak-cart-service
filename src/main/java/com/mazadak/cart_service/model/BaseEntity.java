d3aafd720da3762dfe213a8b4b86fb84dcef234f
abdelrahmanMostafa <abdelrahman2772004@gmail.com>
2025-10-09 00:36:53 +0300

    Add BaseEntity class for shared entity properties

---
+++ src/main/java/com/mazadak/cart_service/model/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.mazadak.cart_service.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Data
+public class BaseEntity {
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "updated_by")
+    private String updatedBy;
+}
