cb80f902b7ec43c76ff149ee882117cd5bd80e04
abdelrahmanMostafa <abdelrahman2772004@gmail.com>
2025-10-12 02:05:18 +0300

    feat: add cart controller with REST endpoints

---
+++ src/main/java/com/mazadak/cart_service/controller/CartController.java
@@ -0,0 +1,71 @@
+package com.mazadak.cart_service.controller;
+
+import com.mazadak.cart_service.dto.request.AddItemRequest;
+import com.mazadak.cart_service.dto.request.UpdateItemRequest;
+import com.mazadak.cart_service.dto.response.CartItemResponseDTO;
+import com.mazadak.cart_service.service.CartService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Validated
+@RestController
+@RequestMapping("/carts")
+@RequiredArgsConstructor
+public class CartController {
+    private final CartService cartService;
+
+    @PostMapping("/items")
+    public ResponseEntity<CartItemResponseDTO> addItem(
+            @RequestHeader("user-id")  @NotNull(message = "User ID is required") UUID userId,
+            @Valid @RequestBody AddItemRequest addItemRequest) {
+
+        return ResponseEntity.ok(cartService.addItem(userId, addItemRequest));
+    }
+
+    @DeleteMapping("/items/{productId}")
+    public ResponseEntity<Void> removeItem(
+            @RequestHeader("user-id") @NotNull(message = "User ID is required") UUID userId,
+            @PathVariable @NotNull(message = "Product ID is required") UUID productId) {
+
+        cartService.removeItem(userId, productId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/items")
+    public ResponseEntity<CartItemResponseDTO> updateItemQuantity(
+            @RequestHeader("user-id") @NotNull(message = "User ID is required") UUID userId,
+            @Valid @RequestBody UpdateItemRequest updateItemRequest) {
+
+        return ResponseEntity.ok(cartService.updateItemQuantity(userId, updateItemRequest));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CartItemResponseDTO>> getCartItems(
+            @RequestHeader("user-id") @NotNull(message = "User ID is required") UUID userId) {
+
+        return ResponseEntity.ok(cartService.getCartItems(userId));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> clearCart(
+            @RequestHeader("user-id") @NotNull(message = "User ID is required") UUID userId) {
+
+        cartService.clearCart(userId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/items/reduce/{productId}")
+    public ResponseEntity<CartItemResponseDTO> reduceItemQuantity(
+            @RequestHeader("user-id") @NotNull(message = "User ID is required") UUID userId,
+            @Valid @PathVariable UUID productId ) {
+
+        return ResponseEntity.ok(cartService.reduceItemQuantity(userId, productId));
+    }
+}
